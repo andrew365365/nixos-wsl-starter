190c6bcec195b9aaf7d2817a4d6d1f48868f5bfe
andy <92188271+andrew365365@users.noreply.github.com>
2023-12-12 12:28:00 -0800

    home.nix changes

--- home.nix
+++ home.nix
@@ -67,12 +67,12 @@
     # language servers
     ccls # c / c++
     gopls
-    # nodePackages.typescript-language-server
-    # pkgs.nodePackages.vscode-langservers-extracted # html, css, json, eslint
-    # nodePackages.yaml-language-server
+    nodePackages.typescript-language-server
+    pkgs.nodePackages.vscode-langservers-extracted # html, css, json, eslint
+    nodePackages.yaml-language-server
     lua-language-server
     nil # nix
-    # nodePackages.pyright
+    nodePackages.pyright
 
     # formatters and linters
     alejandra # nix
@@ -81,7 +81,7 @@
     deadnix # nix
     golangci-lint
     lua52Packages.luacheck
-    # nodePackages.prettier
+    nodePackages.prettier
     shellcheck
     shfmt
     statix # nix
@@ -137,6 +137,7 @@ in {
       ruby.disabled = true;
       hostname.ssh_only = false;
       hostname.style = "bold green";
+      username.show_always = true;
     };
 
     # FIXME: disable whatever you don't want
@@ -188,152 +189,6 @@ in {
 	rebase = true;
 	};
       };
-    };
-
-    # FIXME: This is my .zshrc - you can fiddle with it if you want
-    zsh = {
-      enable = true;
-      autocd = true;
-      enableAutosuggestions = true;
-      enableCompletion = true;
-      defaultKeymap = "emacs";
-      history.size = 10000;
-      history.save = 10000;
-      history.expireDuplicatesFirst = true;
-      history.ignoreDups = true;
-      history.ignoreSpace = true;
-      historySubstringSearch.enable = true;
-
-      plugins = [
-        {
-          name = "fast-syntax-highlighting";
-          src = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
-        }
-	{
-	  name = "zsh-history-substring-search";
-	  src = "${pkgs.zsh-history-substring-search}/share/zsh/site-functions";
-	}
-        {
-          name = "zsh-nix-shell";
-          file = "nix-shell.plugin.zsh";
-          src = pkgs.fetchFromGitHub {
-            owner = "chisui";
-            repo = "zsh-nix-shell";
-            rev = "v0.5.0";
-            sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
-          };
-        }
-      ];
-
-      shellAliases = {
-        "..." = "./..";
-        "...." = "././..";
-        cd = "z";
-        gc = "nix-collect-garbage --delete-old";
-        refresh = "source ${config.home.homeDirectory}/.zshrc";
-        show_path = "echo $PATH | tr ':' '\n'";
-
-        # FIXME: add more git aliases here if you want them
-        gapa = "git add --patch";
-        grpa = "git reset --patch";
-        gst = "git status";
-        gdh = "git diff HEAD";
-        gp = "git push";
-        gph = "git push -u origin HEAD";
-        gco = "git checkout";
-        gcob = "git checkout -b";
-        gcm = "git checkout master";
-        gcd = "git checkout develop";
-
-        pbcopy = "/mnt/c/Windows/System32/clip.exe";
-        pbpaste = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -command 'Get-Clipboard'";
-        explorer = "/mnt/c/Windows/explorer.exe";
-      };
-
-      envExtra = ''
-        export PATH=$PATH:$HOME/.local/bin
-      '';
-
-      initExtra = ''
-        bindkey '^p' history-search-backward
-        bindkey '^n' history-search-forward
-        bindkey '^e' end-of-line
-        bindkey '^w' forward-word
-        bindkey "^[[3~" delete-char
-        bindkey ";5C" forward-word
-        bindkey ";5D" backward-word
-
-        zstyle ':completion:*:*:*:*:*' menu select
-
-        # Complete . and .. special directories
-        zstyle ':completion:*' special-dirs true
-
-        zstyle ':completion:*' list-colors ""
-        zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-
-        # disable named-directories autocompletion
-        zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
-
-        # Use caching so that commands like apt and dpkg complete are useable
-        zstyle ':completion:*' use-cache on
-        zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
-
-        # Don't complete uninteresting users
-        zstyle ':completion:*:*:*:users' ignored-patterns \
-                adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
-                clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
-                gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
-                ldap lp mail mailman mailnull man messagebus  mldonkey mysql nagios \
-                named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
-                operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
-                rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
-                usbmux uucp vcsa wwwrun xfs '_*'
-        # ... unless we really want to.
-        zstyle '*' single-ignored complete
-
-        # https://thevaluable.dev/zsh-completion-guide-examples/
-        zstyle ':completion:*' completer _extensions _complete _approximate
-        zstyle ':completion:*:descriptions' format '%F{green}-- %d --%f'
-        zstyle ':completion:*' group-name ""
-        zstyle ':completion:*:*:-command-:*:*' group-order alias builtins functions commands
-        zstyle ':completion:*' squeeze-slashes true
-        zstyle ':completion:*' matcher-list "" 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-
-        # mkcd is equivalent to takedir
-        function mkcd takedir() {
-          mkdir -p $@ && cd ''${@:$#}
-        }
-
-        function takeurl() {
-          local data thedir
-          data="$(mktemp)"
-          curl -L "$1" > "$data"
-          tar xf "$data"
-          thedir="$(tar tf "$data" | head -n 1)"
-          rm "$data"
-          cd "$thedir"
-        }
-
-        function takegit() {
-          git clone "$1"
-          cd "$(basename ''${1%%.git})"
-        }
-
-        function take() {
-          if [[ $1 =~ ^(https?|ftp).*\.(tar\.(gz|bz2|xz)|tgz)$ ]]; then
-            takeurl "$1"
-          elif [[ $1 =~ ^([A-Za-z0-9]\+@|https?|git|ssh|ftps?|rsync).*\.git/?$ ]]; then
-            takegit "$1"
-          else
-            takedir "$@"
-          fi
-        }
-
-        WORDCHARS='*?[]~=&;!#$%^(){}<>'
-
-        # fixes duplication of commands when using tab-completion
-        export LANG=C.UTF-8
-      '';
     };
   };
 }
