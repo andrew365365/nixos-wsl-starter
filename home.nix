2eeaf8590bbb05bfc0c06cfd17a0b019e23846b4
andy <92188271+andrew365365@users.noreply.github.com>
2023-12-18 10:59:24 -0800

    home.nix remove git and delta from home manager:wq

--- home.nix
+++ home.nix
@@ -70,12 +70,12 @@
     # language servers
     ccls # c / c++
     gopls
-    nodePackages.typescript-language-server
-    pkgs.nodePackages.vscode-langservers-extracted # html, css, json, eslint
-    nodePackages.yaml-language-server
+    nodepackages.typescript-language-server
+    pkgs.nodepackages.vscode-langservers-extracted # html, css, json, eslint
+    nodepackages.yaml-language-server
     lua-language-server
     nil # nix
-    nodePackages.pyright
+    nodepackages.pyright
     bash
 
     # formatters and linters
@@ -84,8 +84,8 @@
     ruff # python
     deadnix # nix
     golangci-lint
-    lua52Packages.luacheck
-    nodePackages.prettier
+    lua52packages.luacheck
+    nodepackages.prettier
     shellcheck
     shfmt
     statix # nix
@@ -94,76 +94,56 @@
   ];
 in {
   imports = [
-    nix-index-database.hmModules.nix-index
+    nix-index-database.hmmodules.nix-index
   ];
 
-  home.stateVersion = "22.11";
+  home.stateversion = "22.11";
 
   home = {
     username = "${username}";
-    homeDirectory = "/home/${username}";
+    homedirectory = "/home/${username}";
 
-    sessionVariables.EDITOR = "nvim";
-    sessionVariables.SHELL = "/etc/profiles/per-user/${username}/bin/zsh";
+    sessionvariables.editor = "nvim";
+    sessionvariables.shell = "/etc/profiles/per-user/${username}/bin/zsh";
   };
 
   home.packages =
     stable-packages
     ++ unstable-packages
     ++
-    # FIXME: you can add anything else that doesn't fit into the above two lists in here
+    # fixme: you can add anything else that doesn't fit into the above two lists in here
     [
       # pkgs.some-package
       # pkgs.unstable.some-other-package
     ];
 
-  # FIXME: if you want to version your nvim config, add it to the root of this repo and uncomment the next line
+  # fixme: if you want to version your nvim config, add it to the root of this repo and uncomment the next line
   # home.file.".config/nvim/config.lua".source = ./nvim_config.lua;
 
   programs = {
     home-manager.enable = true;
     nix-index.enable = true;
-    nix-index.enableZshIntegration = true;
+    nix-index.enablezshintegration = true;
     nix-index-database.comma.enable = true;
 
     starship.enable = true;
     starship.settings = {
-      aws.disabled = true;
-      gcloud.disabled = true;
-      kubernetes.disabled = false;
-      git_branch.style = "242";
-      directory.style = "blue";
-      directory.truncate_to_repo = false;
-      directory.truncation_length = 8;
-      python.disabled = true;
-      ruby.disabled = true;
       hostname.ssh_only = false;
       hostname.style = "bold green";
       username.show_always = true;
     };
 
     fzf.enable = true;
-    fzf.enableZshIntegration = true;
+    fzf.enablezshintegration = true;
     lsd.enable = true;
-    lsd.enableAliases = true;
+    lsd.enablealiases = true;
     zoxide.enable = true;
-    zoxide.enableZshIntegration = true;
+    zoxide.enablezshintegration = true;
     broot.enable = true;
-    broot.enableZshIntegration = true;
+    broot.enablezshintegration = true;
 
     direnv.enable = true;
-    direnv.enableZshIntegration = true;
+    direnv.enablezshintegration = true;
     direnv.nix-direnv.enable = true;
-
-    git = {
-      enable = true;
-      package = pkgs.unstable.git;
-      delta.enable = true;
-      delta.options = {
-        line-numbers = true;
-        side-by-side = true;
-        navigate = true;
-      };
-    };
   };
 }
